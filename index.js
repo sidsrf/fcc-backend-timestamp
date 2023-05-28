2b913a28a4373c6f8c9a7769c73d48724227eab0
Siddharth Saraf <sidsrf@gmail.com>
2023-05-28 22:27:33 +0530

    Updated the index.js file

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,11 +18,28 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  let d = req.params['date'];
+  var date;
+  if (isNaN(d)) {
+     date = new Date(d);
+  } else {
+    date = new Date(parseInt(d))
+  }
+  if (date== "Invalid Date") {
+    res.json({"error": "Invalid Date"})
+  } else {
+    res.json({ "unix": date.getTime(), "utc": date.toUTCString() });
+  }
 });
+
+app.get('/api', (req, res) => {
+  let date = new Date();
+  res.json({ "unix": date.getTime(), "utc": date.toUTCString() });
+})
+
+
 
 
 
